8dd36a0a551c0c9d5ad22da1a6b6a72c035d28e1
ThomasDes99 <Thomas99des@mail.com>
2023-08-31 20:52:11 +0700

    Vui oi la vui

--- git.py
+++ git.py
@@ -3,3 +3,5 @@ print("Hello World")
 print("M làm cái miẹ gì git biết hết á! Cho nên đừng có xem Jav nữa")
 
 print("M sửa cái gì cũng được, ngay tại GitHub")
+
+print("Hôm nay mình được ban làm lớp phó ngoại ngữ! Vui quá là vui")
